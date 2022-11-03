be91f4db36cbf3a7052029977e83dff95af4d240
athatyAG <maysoninacio@gmail.com>
2022-11-03 11:41:27 -0300

    site thatyAG teste

---
+++ bd.js
@@ -0,0 +1,14 @@
+var bd = openDatabase("meuBD", "1.0", " Meu Banco de Dados", 4080);
+bd.transaction(function (criar) {
+    criar.executeSql("CREATE TABLE formulario (nome TEXT)");
+});
+
+function salvarNome() {
+    const nomeUsuario = document.getElementById("nome-usuario").value;
+
+    bd.transaction(function (inserir) {
+        inserir.executeSql("INSERT INTO formulario (nome) VALUES (?)", [
+            nomeUsuario,
+        ]);
+    });
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+function converteParaFahrenheit() {
+    const tempCelsius = parseFloat(
+        document.getElementById("temperatura-celsius").value
+    );
+
+    const tempFahrenheit = tempCelsius * 1.8 + 32;
+
+    document.getElementById("resultado-fahrenheit").value =
+        tempFahrenheit.toFixed(2) + "Fahrenheit";
+}
