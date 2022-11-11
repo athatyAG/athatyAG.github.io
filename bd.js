435d09280fd92d6a2b606b15fbaf64424a1c0d5d
athatyAG <athaty96@gmail.com>
2022-11-11 11:58:05 -0300

    adicionando informaçoes no banco de dados

--- bd.js
+++ bd.js
@@ -1,7 +1,9 @@
 var bd = openDatabase("meuBD", "1.0", " Meu Banco de Dados", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formulario (nome TEXT,idade INTEGER)");
+    criar.executeSql(
+        "CREATE TABLE formulario (nome TEXT, idade INTEGER,altura FLOAT(2, 4), dataNasc DATE  , contatos JSON)"
+    );
 });
 
 function salvarInfo() {
@@ -10,25 +12,48 @@ function salvarInfo() {
         .value.toUpperCase();
     const idadeUsuario = parseInt(
         document.getElementById("idade-usuario").value
-    )
-    
-    ;
+    );
+
+    const altura = parseFloat(document.getElementById("altura-usuario").value);
+    const dataNasc = document.getElementById("data-nasc-usuario").value;
+
+    const emailUsuario = document.getElementById("email-usuario").value;
+    const telefoneUsuario = document.getElementById("telefone-usuario").value;
+
+    const contatos = { "e-mail": emailUsuario, tel: telefoneUsuario };
 
     if (nomeUsuario === "" || isNaN(idadeUsuario)) {
         alert("Faltam informaçoes!");
         return false;
     }
-
+    console.log([
+        nomeUsuario,
+        idadeUsuario,
+        altura,
+        dataNasc,
+        JSON.stringify(contatos),
+    ]);
     bd.transaction(function (inserir) {
         inserir.executeSql(
-            "INSERT INTO formulario (nome, idade) VALUES (?, ?)",
-            [nomeUsuario, idadeUsuario]
+            "INSERT INTO formulario (nome, idade, altura, dataNasc, contatos) VALUES (?, ?, ?, ?, ?)",
+            [
+                nomeUsuario,
+                idadeUsuario,
+                altura,
+                dataNasc,
+                JSON.stringify(contatos),
+            ]
         );
     });
-    console.log(nomeUsuario, idadeUsuario);
+    console.log(nomeUsuario, idadeUsuario, altura, dataNasc, contatos);
 
     document.getElementById("nome-usuario").value = "";
     document.getElementById("idade-usuario").value = "";
+    document.getElementById("altura-usuario").value = "";
+    document.getElementById("data-nasc-usuario").value = "";
+
+    document.getElementById("email-usuario").value = "";
+    document.getElementById("telefone-usuario").value = "";
 }
 
 function pesquisaPorNome() {
@@ -48,8 +73,12 @@ function pesquisaPorNome() {
 
                 const nome = resultados.rows.item(tamanho - 1).nome;
                 const idade = resultados.rows.item(tamanho - 1).idade;
+                const cpf = resultados.rows.item(-1).cpf;
+                const email = resultados.rows.item(-1).email;
                 document.getElementById("pesquisa-nome-usuario").value = nome;
                 document.getElementById("resuldado-pesquisa").value = idade;
+                document.getElementById("pesquisa-cpf-usuario").value = cpf;
+                document.getElementById("pesquisa-email-usuario").value = email;
             }
         );
     });
@@ -70,7 +99,7 @@ function exibeBD() {
                     item = resultados.rows.item(i);
                     document.getElementById(
                         "lista-bd"
-                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}',${item.idade})"> Nome: ${item.nome}, ${item.idade} anos</p>`;
+                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}',${item.idade},'${item.cpf}','${item.email})"> Nome: ${item.nome}, ${item.idade} anos</p>`;
                 }
             }
         );
